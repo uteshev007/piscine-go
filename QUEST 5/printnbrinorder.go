842b5f168c63f84d6dd327485078caca66e0849d
Ashton <askhatuteshev@gmail.com>
2023-07-08 01:07:52 +0600

    done

---
+++ QUEST 5/printnbrinorder.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	var cnt [10]int
+	for n != 0 {
+		cnt[n%10]++
+		n /= 10
+	}
+	for i := 0; i < 10; i++ {
+		for cnt[i] > 0 {
+			z01.PrintRune(rune(i) + '0')
+			cnt[i]--
+		}
+	}
+}
+
+func main() {
+	PrintNbrInOrder(321)
+	PrintNbrInOrder(0)
+	PrintNbrInOrder(321)
+}
